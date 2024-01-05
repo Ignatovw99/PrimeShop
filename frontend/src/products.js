24be204b4dd3f224fb82b13b6b0b70e0edb9fb52
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-01-05 20:36:13 +0200

    products data file added

---
+++ frontend/src/products.js
@@ -0,0 +1,83 @@
+const products = [
+    {
+      id: '1',
+      name: 'Airpods Wireless Bluetooth Headphones',
+      image: 'https://cdncloudcart.com/402/products/images/111330/slusalki-s-mikrofon-apple-airpods-3rd-gen-mme73-image_61715aa7d0ec8_800x800.jpeg',
+      description:
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+      brand: 'Apple',
+      category: 'Electronics',
+      price: 89.99,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 12,
+    },
+    {
+      id: '2',
+      name: 'iPhone 11 Pro 256GB Memory',
+      image: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6223/6223205_sd.jpg',
+      description:
+        'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      brand: 'Apple',
+      category: 'Electronics',
+      price: 599.99,
+      countInStock: 7,
+      rating: 4.0,
+      numReviews: 8,
+    },
+    {
+      id: '3',
+      name: 'Cannon EOS 80D DSLR Camera',
+      image: 'https://magazin.photosynthesis.bg/141359-large_default/fotoaparat-canon-eos-80d-obektiv-canon-ef-s-18-55mm-is-stm.jpg',
+      description:
+        'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+      brand: 'Cannon',
+      category: 'Electronics',
+      price: 929.99,
+      countInStock: 5,
+      rating: 3,
+      numReviews: 12,
+    },
+    {
+      id: '4',
+      name: 'Sony Playstation 4 Pro White Version',
+      image: 'https://pcmall.bg/products/or74289_2.jpg',
+      description:
+        'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+      brand: 'Sony',
+      category: 'Electronics',
+      price: 399.99,
+      countInStock: 11,
+      rating: 5,
+      numReviews: 12,
+    },
+    {
+      id: '5',
+      name: 'Logitech G-Series Gaming Mouse',
+      image: 'https://resource.logitechg.com/w_692,c_lpad,ar_4:3,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/gaming/en/products/g502-lightspeed-gaming-mouse/g502-lightspeed-gallery-1.png?v=1',
+      description:
+        'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+      brand: 'Logitech',
+      category: 'Electronics',
+      price: 49.99,
+      countInStock: 7,
+      rating: 3.5,
+      numReviews: 10,
+    },
+    {
+      id: '6',
+      name: 'Amazon Echo Dot 3rd Generation',
+      image: 'https://cdn.mos.cms.futurecdn.net/swDXRU36UHKwg8LgxLEvn6.jpg',
+      description:
+        'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+      brand: 'Amazon',
+      category: 'Electronics',
+      price: 29.99,
+      countInStock: 0,
+      rating: 4,
+      numReviews: 12,
+    },
+  ];
+  
+  export default products;
+  
