bdcd26a35128cae44111cdd46680bea73c83ef6c
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-01-12 13:43:50 +0200

    sample data added

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        name: 'PrimeShop Admin',
+        email: 'admin@primeshop.com',
+        password: 'password123',
+    },
+    {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        password: 'password123',
+    },
+    {
+        name: 'Jane Smith',
+        email: 'jane.smith@example.com',
+        password: 'password123',
+    },
+    {
+        name: 'Bob Johnson',
+        email: 'bob.johnson@example.com',
+        password: 'password123',
+    },
+];
+
+export default users;
