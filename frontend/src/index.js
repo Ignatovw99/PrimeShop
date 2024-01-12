7920c0c3fda4642b2aede670ef57835014130d93
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-01-12 13:40:39 +0200

    router created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider
+} from 'react-router-dom'
 
 import App from './App';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<App />}>
+            <Route index={true} element={<HomePage />} />
+            <Route path="/products/:id" element={<ProductPage />} />
+        </Route>
+    )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
 
