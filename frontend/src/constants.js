6ccb34130eddf81b415e654228e9fda877e3a038
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-01-17 19:20:51 +0200

    shopping cart constants added

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,6 @@
 export const CACHE_UNUSED_API_DATA_SECONDS = 10;
 export const PRODUCTS_URL = "/api/products";
+export const SHOPPING_CART_SLICE_NAME = "shoppingCart";
+export const SHOPPING_CART_STORAGE_KEY = "shoppingCart";
 export const PRODUCT_IN_STOCK = "In Stock";
 export const PRODUCT_OUT_OF_STOCK = "Out Of Stock";
