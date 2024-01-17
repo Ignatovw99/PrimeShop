6a8d77a948d8acde13318edaa89a0f39110e3662
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-01-17 19:10:51 +0200

    fetch products from API slice and show loader or alert message in home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,28 +1,38 @@
-import { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 
-import axios from "axios";
-
 import Product from "../components/Product";
+import AlertMessage from "../components/AlertMessage";
+import Loader from "../components/Loader";
+
+import { useGetProductsQuery } from "../slices/api/productApiSlice";
 
 const HomePage = () => {
-    const [products, setProducts] = useState([]);
+    const { data: products, isFetching, error } = useGetProductsQuery();
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get("/api/products");
-            setProducts(data);
-        };
-        
-        fetchProducts();
-    }, []);
+    if (error) {
+        return (
+            <AlertMessage variant="danger">
+                {error.data?.message}
+            </AlertMessage>
+        );
+    }
+
+    if (isFetching) {
+        return <Loader />;
+    }
 
     return (
         <>
             <h1>Leatest Products</h1>
             <Row>
                 {products.map(product =>
-                    <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+                    <Col
+                        key={product.id}
+                        sm={12}
+                        md={6}
+                        lg={4}
+                        xl={3}
+                    >
                         <Product product={product} />
                     </Col>
                 )}
