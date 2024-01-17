7b25784e13b6507b2308b8f134d7db5ee8929a2d
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-01-17 19:11:44 +0200

    in and out of stock constants added

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,4 @@
 export const CACHE_UNUSED_API_DATA_SECONDS = 10;
 export const PRODUCTS_URL = "/api/products";
+export const PRODUCT_IN_STOCK = "In Stock";
+export const PRODUCT_OUT_OF_STOCK = "Out Of Stock";
