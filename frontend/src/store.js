42cf2ff97e5cd02912af601858ba20cf319c5473
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-01-18 20:43:54 +0200

    shopping cart reducer integrated in state storage and state synchronization

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,17 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { createStateSyncMiddleware, initMessageListener } from "redux-state-sync";
 
 import apiSlice from "./slices/api/apiSlice";
+import shoppingCartReducer, { actionTypes } from "./slices/shoppingCartSlice";
 
 import { ENABLE_REDUX_DEV_TOOLS } from "./config";
 
 const store = configureStore({
     reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        shoppingCart: shoppingCartReducer
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware()
-            .concat(apiSlice.middleware),
+            .concat(apiSlice.middleware)
+            .concat(createStateSyncMiddleware({ whitelist: actionTypes})),
     devTools: ENABLE_REDUX_DEV_TOOLS
 });
+
+initMessageListener(store);
 
 export default store;
