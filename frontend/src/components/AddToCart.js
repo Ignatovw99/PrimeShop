e5b50ce176bfe5eef4e62d4619105cf86f714008
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-01-19 10:55:07 +0200

    add to cart component

---
+++ frontend/src/components/AddToCart.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Row, Col, ListGroup, Button } from "react-bootstrap";
+
+import ProductQuantitySelect from "./ProductQuantitySelect";
+
+import { addItemToCart } from "../slices/shoppingCartSlice";
+
+const AddToCart = ({ product }) => {
+    const [quantity, setQuantity] = useState(1);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const addToCartHandler = () => {
+        dispatch(addItemToCart({ ...product, quantity }));
+        navigate("/shopping-cart");
+    };
+
+    const productInStock = product.availableQuantity > 0;
+
+    return (
+        <>
+            <ListGroup variant="flush">
+                {productInStock &&
+                    <ListGroup.Item>
+                        <Row>
+                            <Col>Quantity</Col>
+                            <Col>
+                                <ProductQuantitySelect
+                                    quantity={product.quantity}
+                                    availableQuantity={product.availableQuantity}
+                                    changeQuantityHandler={event => setQuantity(Number(event.target.value))}
+                                />
+                            </Col>
+                        </Row>
+                    </ListGroup.Item>
+                }
+
+                <ListGroup.Item>
+                    <Button
+                        className="btn-block"
+                        disabled={!productInStock}
+                        onClick={addToCartHandler}
+                    >
+                        Add to Cart
+                    </Button>
+                </ListGroup.Item>
+            </ListGroup>
+        </>
+    );
+};
+
+export default AddToCart;
