66da2cdbd6ca8d71487bf649dfab71f01fd83384
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-01-20 18:51:02 +0200

    secret and environment properties added

--- backend/src/config/properties.js
+++ backend/src/config/properties.js
@@ -6,6 +6,12 @@ export default {
         url: process.env.MONGO_URL || "mongodb://localhost:27017/primeshop-db"
     },
     auth: {
-        saltRounds: process.env.SALT_ROUNDS || 10
+        saltRounds: process.env.SALT_ROUNDS || 10,
+        secret: process.env.JWT_SECRET,
+        accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN || "1h"
+    },
+    environemnt: {
+        isProduction: process.env.NODE_ENV === "production",
+        isDevelopment: process.env.NODE_ENV === "development"
     }
 };
