5c7c1cea4e8d52f2e0a8ebae0892185125f56be8
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-01-22 15:53:23 +0200

    errors module added

---
+++ backend/src/errors.js
@@ -0,0 +1,37 @@
+import { replacePlaceholders } from "./utils/common.js";
+
+export class NotFoundError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = this.constructor.name;
+    }
+
+    static throwError(messageTemplate, replacements) {
+        const message = replacePlaceholders(messageTemplate, replacements);
+        throw new NotFoundError(message);
+    }
+}
+
+export class DuplicateError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = this.constructor.name;
+    }
+
+    static throwError(messageTemplate, replacements) {
+        const message = replacePlaceholders(messageTemplate, replacements);
+        throw new NotFoundError(message);
+    }
+}
+
+export class ServerError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = this.constructor.name;
+    }
+
+    static throwError(messageTemplate, replacements) {
+        const message = replacePlaceholders(messageTemplate, replacements);
+        throw new ServerError(message);
+    }
+}
