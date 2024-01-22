924a6b5bff29f023752232635df83f5827bd03d8
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-01-22 15:31:47 +0200

    server module added

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import http from "node:http";
+
+import app from "./src/app.js";
+import { connectDatabase } from "./src/config/database.js";
+
+import configProperties from "./src/config/properties.js";
+
+const port = configProperties.server.port;
+const server = http.createServer(app);
+
+const startServer = async () => {
+    try {
+        await connectDatabase();
+        server.listen(port, () => console.log(`Server is running on port ${port}`));
+    } catch (error) {
+        console.log('The application cannot be started', error);
+    }
+};
+
+startServer();
