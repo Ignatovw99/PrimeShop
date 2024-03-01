3083bb29053089dada756bb3d0ed4761dfa07d05
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-03-01 15:46:04 +0200

    checkout handler added in the cart page

--- frontend/src/pages/ShoppingCartPage.js
+++ frontend/src/pages/ShoppingCartPage.js
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Row, Col, ListGroup, Card, Button } from 'react-bootstrap';
@@ -8,8 +9,10 @@ import ShoppingCartItem from "../components/ShoppingCartItem";
 import { getShoppingCartItemsQuantity } from "../utils/shoppingCartUtils";
 
 const ShoppingCartPage = () => {
-    const shoppingCart = useSelector(state => state.shoppingCart);
-    const { itemsPrice, items: cartItems } = shoppingCart;
+    const { itemsPrice, items: cartItems } = useSelector(state => state.shoppingCart);
+    const navigate = useNavigate();
+
+    const checkoutHandler = () => navigate("/shipping");
 
     const isShoppingCartEmpty = cartItems.length === 0;
 
@@ -17,12 +20,11 @@ const ShoppingCartPage = () => {
         <Row>
             <Col md={8}>
                 <h1 className="shopping-cart-title">Shopping Cart</h1>
-                {isShoppingCartEmpty &&
+                {isShoppingCartEmpty ? (
                     <AlertMessage>
                         Your shopping cart is empty <Link to="/">Go Back</Link>
                     </AlertMessage>
-                }
-                {!isShoppingCartEmpty &&
+                ) : (
                     <ListGroup variant="flush">
                         {cartItems.map(item =>
                             <ListGroup.Item key={item.id}>
@@ -30,7 +32,7 @@ const ShoppingCartPage = () => {
                             </ListGroup.Item>
                         )}
                     </ListGroup>
-                }
+                )}
             </Col>
             <Col md={4}>
                 <Card>
@@ -48,6 +50,7 @@ const ShoppingCartPage = () => {
                                 type="button"
                                 variant="dark"
                                 disabled={isShoppingCartEmpty}
+                                onClick={checkoutHandler}
                             >
                                 Proceed To Checkout
                             </Button>
