062b2decb727fb206d3bb344f1b48ba1643d68ec
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-03-05 20:26:37 +0200

    radio button input field component created

---
+++ frontend/src/components/RadioButtonInput.js
@@ -0,0 +1,34 @@
+import { Form, Col } from "react-bootstrap";
+
+const RadioButtonInput = ({
+    label,
+    options,
+    name,
+    className,
+    checkedValue,
+    onChangeHandler
+}) => {
+    return (
+        <Form.Group>
+            <Form.Label as="legend">
+                {label}
+            </Form.Label>
+            <Col>
+                {options.map(option => (
+                    <Form.Check
+                        type="radio"
+                        className={className}
+                        label={option.label}
+                        id={option.value}
+                        name={name}
+                        value={option.value}
+                        checked={option.value === checkedValue}
+                        onChange={onChangeHandler}
+                    />
+                ))}
+            </Col>
+        </Form.Group>
+    );
+};
+
+export default RadioButtonInput;
