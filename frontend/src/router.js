d0b56a18c93367c8dc125d087b3f928b1b2ba03f
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-03-05 20:28:11 +0200

    private routes added

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -6,11 +6,15 @@ import {
 
 import App from './App';
 
+import PrivateRoute from './routes/PrivateRoute';
+
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import ShoppingCartPage from './pages/ShoppingCartPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ShippingPage from './pages/ShippingPage';
+import PaymentPage from './pages/PaymentPage';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -20,6 +24,11 @@ const router = createBrowserRouter(
             <Route path="/shopping-cart" element={<ShoppingCartPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+
+            <Route path="" element={<PrivateRoute />}>
+                <Route path="/shipping" element={<ShippingPage />} />
+                <Route path="/payment" element={<PaymentPage />} />
+            </Route>
         </Route>
     )
 );
